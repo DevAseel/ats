33ffc885db7d890835aef61a3f4df2d51c912e3c
Aseel Al Rawahi <dev.aseel@outlook.com>
2022-06-03 20:24:03 +0400

    integrated prisma client and mongodb

--- endpoints/healthCheck.ts
+++ endpoints/healthCheck.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const healthCheck = (req: Request, res: Response) => {
+export const healthCheck = async (req: Request, res: Response) => {
 	res.status(200).send("API is healthy");
 };
