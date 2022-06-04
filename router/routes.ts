673ebc790fd24824849925ef2efe3ebe3dde5c49
Aseel Al Rawahi <dev.aseel@outlook.com>
2022-06-04 15:17:39 +0400

    Added swagger docs

--- router/routes.ts
+++ router/routes.ts
@@ -2,7 +2,27 @@ import { Router } from "express";
 import { healthCheck } from "../endpoints/healthCheck";
 
 const router = Router();
-
+/**
+ * @swagger
+ * /api/health-check:
+ *  post:
+ *      summary: create a new agent
+ *      tags: [Agent]
+ *      description: use to create a new agent
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/AgentIDTO'
+ *      responses:
+ *          '200':
+ *              description: Agent created successfully
+ *          '400':
+ *                  description: Bad Request
+ *          '500':
+ *                  description: Internal server error
+ */
 router.route("/health-check").get(healthCheck);
 
 export default router;
