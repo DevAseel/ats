83ad92917b83a685e5ae5f14e1b0952e2710bd73
Aseel Al Rawahi <99526005+DevAseel@users.noreply.github.com>
2022-06-04 15:18:51 +0400

    Added swagger docs (#5)

--- endpoints/healthCheck.ts
+++ endpoints/healthCheck.ts
@@ -2,6 +2,21 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+/**
+ * @openapi
+ * /api/health-check:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Server health status
+ *     responses:
+ *       200:
+ *         description: Server is up and running
+ */
+
 export const healthCheck = async (req: Request, res: Response) => {
-	res.status(200).send("API is healthy");
+	res.status(200).json({
+		status: 200,
+		msg: "API is healthy",
+	});
 };
